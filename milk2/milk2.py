c43623cf206696a53c507283886e925b0d44884f
Jason Huh <jasonhuh@hotmail.com>
2019-03-06 07:21:37 -0800

    Update milk2.py

--- milk2/milk2.py
+++ milk2/milk2.py
@@ -9,26 +9,44 @@ LANG: PYTHON3
 TASK: milk2
 """
 
+"""
+ID: jasonhu5
+LANG: PYTHON3
+TASK: milk2
+"""
+
+"""
+ID: jasonhu5
+LANG: PYTHON3
+TASK: milk2
+"""
+
 def solve(ar):
+    n = len(ar)
     ar = sorted(ar, key=lambda x: x[1]) # Sort by finishing time
     max_yes, max_no = ar[0][1] - ar[0][0], 0
     j = 0 # Index of starting activity
+    start = ar[0][0]
     for i, cur in enumerate(ar[:-1]):
         nxt = ar[i+1]
-        if cur[1] < nxt[0]: # No overlap
-            yes = max(cur[1] - ar[j][0] # Duration for continuous activities
-            max_yes = max(max_yes, yes)
+        yes = nxt[1] - nxt[0]
+        if cur[1] >= nxt[0]: # Overlap
+            start = min(start, nxt[0])
+            yes = max(yes, nxt[0] - start)
+        else:
+            yes = max(yes, cur[1] - start) # Duration for continuous activities
             no = nxt[0] - cur[1] # Duration for no activity
             max_no = max(max_no, no)
-            j = i + 1
-        # TODO: Handle the enclosed scenario
+            print(max_no, no)
+        max_yes = max(max_yes, yes)            
     return (max_yes, max_no)
 
 def test_simple():
     assert solve([(100, 200)]) == (100, 0)
+    assert solve([(300, 1000), (700, 1200), (1500, 2100)]) == (900, 300)
     assert solve([(2, 3), (4, 5), (6, 7), (8, 9), (10, 11), (12, 13), \
                     (14, 15), (16, 17), (18, 19), (1, 20)]) == (19, 0)
-    assert solve([(300, 1000), (700, 1200), (1500, 2100)]) == (900, 300)
+    
 
 if __name__ == '__main__':
     test_simple()
