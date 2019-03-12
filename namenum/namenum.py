7be7e59eaeae90922997066a6e1d740470bb77bd
Jason Huh <jasonhuh@hotmail.com>
2019-03-12 06:29:11 -0700

    Accepted solution

--- namenum/namenum.py
+++ namenum/namenum.py
@@ -1,7 +1,8 @@
-# This is a work-in-progress
-
-keypad_mapping = {2:'ABC',3:'DEF',4:'GHI',5:'JKL',6:'MNO',7:'PRS',8:'TUV',9:'WXY'}
-
+"""
+ID: jasonhu5
+LANG: PYTHON3
+TASK: namenum
+"""
 
 class Node(object):
     def __init__(self, char : str):
@@ -28,7 +29,11 @@ class Trie(object):
                 node = new_node
         node.is_end_of_word = True
 
-    def find(self, word):
+    def find(self, word, partial_search):
+        """
+        partial_search is a flag to allow a prefix search if True. If the flag is false,
+        the method will return True only if the result is an extact match.
+        """
         node = self.root
         for c in word:
             found = False
@@ -39,32 +44,53 @@ class Trie(object):
                     break
             if not found:
                 return False
-        return True
+        if partial_search:
+            return True
+        return node.is_end_of_word
 
-def generate_name(serial_no, idx=None, sofar=None):
-    if idx is None:
-        generate_name(serial_no, 0, '')
-    else:
-        if idx == len(serial_no):
-            print(sofar)
-            return
-        cur_digit = int(serial_no[idx])
-        if cur_digit in keypad_mapping:
-            for c in keypad_mapping[cur_digit]:
-                # TODO: Filter using trie
-                if trie.find(sofar+c):
-                    generate_name(serial_no, idx+1, sofar+c)
-        else: 
-            # keypad mapping does not contain the current digit of serial number.
-            # Just move to the next digit with the current result so far.
-            if trie.find(sofar):
-                generate_name(serial_no, idx+1, sofar)
+def solve(serial_no):
+    def load_names(trie):
+        fin = open('dict.txt', 'r')
+        for line in fin.readlines():
+            trie.add(line.strip())
 
-def load_names(trie):
-    fin = open('dict.txt', 'r')
-    for line in fin.readlines():
-        trie.add(line.strip())
+    def generate_name(serial_no, idx=None, sofar=None):
+        if idx is None:
+            generate_name(serial_no, 0, '')
+        else:
+            if idx == len(serial_no):
+                if trie.find(sofar, False):
+                    output.append('{}\n'.format(sofar))
+                return
+            cur_digit = int(serial_no[idx])
+            if cur_digit in keypad_mapping:
+                for c in keypad_mapping[cur_digit]:
+                    # TODO: Filter using trie
+                    if trie.find(sofar+c, True):
+                        generate_name(serial_no, idx+1, sofar+c)
+            else: 
+                # keypad mapping does not contain the current digit of serial number.
+                # Just move to the next digit with the current result so far.
+                if trie.find(sofar, True):
+                    generate_name(serial_no, idx+1, sofar)
 
-trie = Trie()
-load_names(trie)
-generate_name('4734')
+    keypad_mapping = {2:'ABC',3:'DEF',4:'GHI',5:'JKL',6:'MNO',7:'PRS',8:'TUV',9:'WXY'}
+    trie = Trie()
+    load_names(trie)
+    output = []
+    generate_name(serial_no)
+    if len(output) == 0:
+        output.append('NONE\n')
+    return sorted(output)
+
+def test_simple():
+    assert solve('4734') == ['GREG\n']
+    assert solve('234643') == ['NONE\n']
+    assert solve('223') == ['ABE\n', 'ACE\n']
+
+if __name__ == '__main__':
+    fin = open('namenum.in','r')
+    fout = open('namenum.out','w')
+    serial_no = fin.readline().strip()
+    ans = solve(serial_no)
+    fout.write(''.join(ans))
